fbbb3f9448861d71a4f36e4b249d7841816b7bca
Trustin Lee <t@motd.kr>
2019-06-30 17:13:58 +0900

    Update sphinx-maven-plugin from 2.4.0 to 2.5.0

--- src/site/sphinx/conf.py
+++ src/site/sphinx/conf.py
@@ -21,8 +21,8 @@ with open('test.yaml', 'r') as stream:
 
 project = u'sphinx-gradle-plugin'
 copyright = u'2017, Trustin Lee et al'
-version = '2.4'
-release = '2.4.0'
+version = '2.5'
+release = '2.5.0'
 
 # General options
 needs_sphinx = '1.0'
