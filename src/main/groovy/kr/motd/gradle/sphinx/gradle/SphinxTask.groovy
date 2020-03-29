952cbcba9819836608cae3c4fc22206dc9f38702
Trustin Lee <t@motd.kr>
2020-03-29 13:49:46 +0900

    Update dependencies / Fix deprecation warnings
    - sphinx-maven-plugin 2.6.1 -> 2.7.0
    - Gradle 6.3
    - Use new build configurations (compile -> implementation)
    - Fix deprecation warnings:
      - Property 'X' has redundant getters: 'getX()' and 'isX()'.
      - Property 'X' has @Input annotation used on property of type 'File'.
      - Property 'X' is not annotated with an input or output annotation.

--- src/main/groovy/kr/motd/gradle/sphinx/gradle/SphinxTask.groovy
+++ src/main/groovy/kr/motd/gradle/sphinx/gradle/SphinxTask.groovy
@@ -11,25 +11,25 @@ import java.util.concurrent.Callable
 
 class SphinxTask extends DefaultTask {
 
-    def binaryUrl = { SphinxRunner.DEFAULT_BINARY_URL }
-    def binaryCacheDir = { "${project.gradle.gradleUserHomeDir}/caches/sphinx-binary" }
-    def environments = { Collections.emptyMap() }
+    private def binaryUrl = { SphinxRunner.DEFAULT_BINARY_URL }
+    private def binaryCacheDir = { "${project.gradle.gradleUserHomeDir}/caches/sphinx-binary" }
+    private def environments = { Collections.emptyMap() }
     private final def additionalEnvironments = new HashMap<String, String>()
-    def dotBinary = null
-    def sourceDirectory = {
+    private def dotBinary = null
+    private def sourceDirectory = {
         "${project.projectDir}${File.separator}src${File.separator}site${File.separator}sphinx"
     }
-    def outputDirectory = { "${project.buildDir}${File.separator}site" }
-    def doctreeCacheDirectory = { "${project.buildDir}${File.separator}site${File.separator}.doctrees" }
-    def builder = { "html" }
-    def tags = { Collections.emptyList() }
+    private def outputDirectory = { "${project.buildDir}${File.separator}site" }
+    private def doctreeCacheDirectory = { "${project.buildDir}${File.separator}site${File.separator}.doctrees" }
+    private def builder = { "html" }
+    private def tags = { Collections.emptyList() }
     private final def additionalTags = new ArrayList<String>()
-    def verbose = { true }
-    def traceback = { true }
-    def force = { false }
-    def warningsAsErrors = { false }
-    def skip = { false }
-    def useDoctreeCache = { false }
+    private def verbose = { true }
+    private def traceback = { true }
+    private def force = { false }
+    private def warningsAsErrors = { false }
+    private def skip = { false }
+    private def useDoctreeCache = { false }
 
     private static def unwrap(Object o) {
         if (o instanceof Callable) {
@@ -44,9 +44,18 @@ class SphinxTask extends DefaultTask {
         return String.valueOf(unwrap(binaryUrl))
     }
 
-    @Input
+    void setBinaryUrl(Object binaryUrl) {
+        this.binaryUrl = binaryUrl
+    }
+
+    @InputDirectory
+    @PathSensitive(PathSensitivity.RELATIVE)
     File getBinaryCacheDir() {
         return project.file(binaryCacheDir).getCanonicalFile()
+    }
+
+    void setBinaryCacheDir(Object binaryCacheDir) {
+        this.binaryCacheDir = binaryCacheDir;
     }
 
     @Input
@@ -61,6 +70,10 @@ class SphinxTask extends DefaultTask {
         env.putAll(unwrapped)
         env.putAll(additionalEnvironments)
         return env
+    }
+
+    void setEnvironments(Object environments) {
+        this.environments = environments
     }
 
     void env(String name, String value) {
@@ -78,6 +91,10 @@ class SphinxTask extends DefaultTask {
         }
     }
 
+    void setDotBinary(Object dotBinary) {
+        this.dotBinary = dotBinary
+    }
+
     @SkipWhenEmpty
     @InputDirectory
     @PathSensitive(PathSensitivity.RELATIVE)
@@ -85,9 +102,17 @@ class SphinxTask extends DefaultTask {
         project.file(sourceDirectory).getCanonicalFile()
     }
 
+    void setSourceDirectory(Object sourceDirectory) {
+        this.sourceDirectory = sourceDirectory
+    }
+
     @OutputDirectory
     File getOutputDirectory() {
         project.file(outputDirectory).getCanonicalFile()
+    }
+
+    void setOutputDirectory(Object outputDirectory) {
+        this.outputDirectory = outputDirectory
     }
 
     @Input
@@ -96,9 +121,17 @@ class SphinxTask extends DefaultTask {
         project.file(doctreeCacheDirectory).getCanonicalFile()
     }
 
+    void setDoctreeCacheDirectory(Object doctreeCacheDirectory) {
+        this.doctreeCacheDirectory = doctreeCacheDirectory
+    }
+
     @Input
     String getBuilder() {
         return String.valueOf(unwrap(builder))
+    }
+
+    void setBuilder(Object builder) {
+        this.builder = builder
     }
 
     @Input
@@ -114,6 +147,10 @@ class SphinxTask extends DefaultTask {
         return result
     }
 
+    void setTags(Object tags) {
+        this.tags = tags
+    }
+
     void tags(String... tags) {
         additionalTags.addAll(tags)
     }
@@ -127,9 +164,17 @@ class SphinxTask extends DefaultTask {
         (verbose instanceof Boolean ? verbose : verbose()).asBoolean()
     }
 
+    void setVerbose(Object verbose) {
+        this.verbose = verbose
+    }
+
     @Input
     boolean isTraceback() {
         (traceback instanceof Boolean ? traceback : verbose()).asBoolean()
+    }
+
+    void setTraceback(Object traceback) {
+        this.traceback = traceback
     }
 
     @Input
@@ -137,9 +182,17 @@ class SphinxTask extends DefaultTask {
         (force instanceof Boolean ? force : force()).asBoolean()
     }
 
+    void setForce(Object force) {
+        this.force = force
+    }
+
     @Input
     boolean isWarningsAsErrors() {
         (warningsAsErrors instanceof Boolean ? warningsAsErrors : warningsAsErrors()).asBoolean()
+    }
+
+    void setWarningsAsErrors(Object warningsAsErrors) {
+        this.warningsAsErrors = warningsAsErrors
     }
 
     @Input
@@ -147,9 +200,17 @@ class SphinxTask extends DefaultTask {
         (useDoctreeCache instanceof Boolean ? useDoctreeCache : useDoctreeCache()).asBoolean()
     }
 
+    void setUseDoctreeCache(Object useDoctreeCache) {
+        this.useDoctreeCache = useDoctreeCache
+    }
+
     @Input
     boolean isSkip() {
         (skip instanceof Boolean ? skip : skip()).asBoolean()
+    }
+
+    void setSkip(Object skip) {
+        this.skip = skip
     }
 
     @TaskAction
